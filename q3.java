9fbc25a962414732ff0d093c82e52b8d504141a3
Onkar-10 <75186056+Onkar-10@users.noreply.github.com>
2021-09-29 05:38:38 +0530

    Add files via upload
    func4 sexyy tha!! (though non overlapping is nice af)..
    func3 bohot ambigous diyaae c ka lol

--- q3.java
+++ q3.java
@@ -1,7 +1,7 @@
 import java.util.HashMap;
 import java.util.regex.Pattern;
 import java.io.* ;
-import java.io.FileWriter;
+import java.util.regex.Matcher;
 import java.util.*;
 
 public class q3
@@ -14,6 +14,16 @@ public class q3
         return (Pattern.matches("a*b+c",s));
     }
     public Boolean func3(String s) {
-        return (Pattern.matches("a+b+",s));
+
+        return (Pattern.matches("",s));  //can c exits
+    }
+    public List<String> func4(String input_s,String pattern_s){
+        List<String> substrings_list = new ArrayList<>();
+        Pattern our_pat = Pattern.compile(pattern_s);
+        Matcher our_mat = our_pat.matcher(input_s);
+    
+        while (our_mat.find())
+           substrings_list.add(our_mat.group());
+        return substrings_list;
     }
 }
